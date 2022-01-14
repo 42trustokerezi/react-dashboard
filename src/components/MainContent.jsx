8b0ab5763bd5352127d09c3d06d9e11d2fca787c
Trust Okerezi <trustokerezi42@gmail.com>
2022-01-14 21:59:21 +0100

    Earnings Card UI

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from "styled-components"
+import Earnings from './Earnings';
 import Navbar from './Navbar';
 
 function MainContent() {
@@ -8,7 +9,9 @@ function MainContent() {
             <Navbar/>
             <SubContainer>
                 <SectionOne>
-                    <Columnone1></Columnone1>
+                    <Columnone1>
+                    <Earnings/>
+                    </Columnone1>
                     <ColumnTwo1></ColumnTwo1>
                 </SectionOne>
                 <SectionTwo>
@@ -20,17 +23,48 @@ function MainContent() {
     )
 }
 
-const Container = styled.div``;
-const SubContainer = styled.div``;
+const Container = styled.div`
+    width: 80%;
+    background: linear-gradient(to bottom right, white 0%right, #e6e4ff 70%);
+    border-bottom-right-radius: 2rem;
+    border-top-right-radius: 2rem;
+    margin: 1rem 8rem 1rem 4rem;
+`;
+const SubContainer = styled.div`
+    margin: 0.5rem 0;
+    height: 80%;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    gap: 4rem;
+`;
 
 const TitleText = styled.h3`
     height: 20%;
 `;
-const SectionOne = styled.div``;
-const SectionTwo = styled.div``;
-const Columnone1 = styled.div``;
+const SectionOne = styled.div`
+    display: flex;
+    justify-content: space-between;
+    height: 40%;
+    gap: 2rem;
+    width: 100%;
+`;
+const SectionTwo = styled.div`
+    display: flex;
+    gap: 2rem;
+    height: 26vh;
+`;
+const Columnone1 = styled.div`
+    display: flex;
+    gap: 3rem;
+`;
 const ColumnOne2 = styled.div``;
-const ColumnTwo1 = styled.div``;
+const ColumnTwo1 = styled.div`
+    display: flex;
+    flex-direction: column;
+    height: 115%;
+    width: 100%;
+`;
 const ColumnTwo2 = styled.div``;
 
 export default MainContent
